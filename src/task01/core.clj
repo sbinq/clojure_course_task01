a42c866f22115901d5cd2146b02ad4efa3a03aae
Stas Binko <stas.binko@gmail.com>
2013-03-16 05:12:58 +0300

    implemented links retrieval

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -2,9 +2,29 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
+;;; can we use advanced functions like tree-seq at this point?
+;;; or the idea is to work more with map/filter/reduce - so we should not skip them?
+(defn all-nodes [data]
+  (tree-seq vector? #(filter vector? (children %)) data))
+
+;;; more "dumb" implementation, not using advanced features
+;;; calling this in the following code, but verified both solutions
+(defn all-nodes-dumb-impl [data]
+  (cons data
+        (reduce concat
+                (map all-nodes-dumb-impl
+                     (filter vector? (children data))))))
+
+(defn link-containers [nodes]
+  (filter (fn [[tag attrs]] (and (= tag :h3) (= (get attrs :class) "r"))) nodes))
+
+(defn container-href [container]
+  ;; can we use threading macros? sticking to simplest clojure features
+  ;; not sure if (-> container children first attributes (get :href)) reads better or worse for me
+  (get (attributes (first (children container))) :href))
 
 (defn get-links []
-" 1) Find all elements containing {:class \"r\"}.
+  " 1) Find all elements containing {:class \"r\"}.
 
 Example:
 [:h3 {:class \"r\"} [:a {:shape \"rect\", :class \"l\",
@@ -20,10 +40,12 @@ The link from the example above is 'https://github.com/clojure/clojure'.
 
 Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
 "
-  (let [data (parse "clojure_google.html")]
-    nil))
+;; (->> (parse "clojure_google.html") all-nodes link-containers (map container-href))
+;; and ->> would be fine here; again, sticking to basic features
+  (let [data (parse "clojure_google.html")
+        nodes (all-nodes-dumb-impl data)
+        containers (link-containers nodes)]
+    (map container-href containers)))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
-
-
