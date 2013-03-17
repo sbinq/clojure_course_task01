4c10d10e84d264746913b4c3c847f9ff0d61faf1
Stas Binko <stas.binko@gmail.com>
2013-03-17 20:15:07 +0300

    reformat get-links doc such way that emacs indentation does not get broken for get-links body

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -24,24 +24,23 @@
   (:href (attributes (first (children container)))))
 
 (defn get-links []
-  " 1) Find all elements containing {:class \"r\"}.
+  "1) Find all elements containing {:class \"r\"}.
 
-Example:
-[:h3 {:class \"r\"} [:a {:shape \"rect\", :class \"l\",
+   Example:
+   [:h3 {:class \"r\"} [:a {:shape \"rect\", :class \"l\",
                          :href \"https://github.com/clojure/clojure\",
                          :onmousedown \"return rwt(this,'','','','4','AFQjCNFlSngH8Q4cB8TMqb710dD6ZkDSJg','','0CFYQFjAD','','',event)\"}
                      [:em {} \"clojure\"] \"/\" [:em {} \"clojure\"] \" · GitHub\"]]
 
    2) Extract href from the element :a.
 
-The link from the example above is 'https://github.com/clojure/clojure'.
+   The link from the example above is 'https://github.com/clojure/clojure'.
 
-  3) Return vector of all 10 links.
+   3) Return vector of all 10 links.
 
-Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
-"
-;; (->> (parse "clojure_google.html") all-nodes link-containers (map container-href))
-;; and ->> would be fine here; again, sticking to basic features
+   Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]"
+  ;; (->> (parse "clojure_google.html") all-nodes link-containers (map container-href))
+  ;; and ->> would be fine here; again, sticking to basic features
   (let [data (parse "clojure_google.html")
         nodes (all-nodes-dumb-impl data)
         containers (link-containers nodes)]
